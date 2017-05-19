52802335fa5f1a6dd9b182e36954468336a2fe35
Neous1 <Neous1@users.noreply.github.com>
2017-05-19 00:22:13 -0400

    setup res req

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 
 var express = require ("express");
 var app = express();
+var path = require("path");
 
-app.set("port", 3000)
+
+app.set("port", 3000);
+
+app.get("/", function(req, res){
+    console.log("GET the homepage");
+    res
+        .status(404)
+        .send("Express yourself");
+});
+
+app.get("/json", function(req, res){
+    console.log("GET the json");
+    res
+        .status(200)
+        .json({"jsonData": true});
+});
+app.get("/file", function(req, res){
+    console.log("GET the file");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, "app.js"));
+});
 
 
-// app.listen(app.get("port"), function(){
-//  console.log("magic happens on port " + app.get("port"));
-// });
 var server = app.listen(app.get("port"), function(){
     var port = server.address().port;
  console.log("magic happens on port " + port);
