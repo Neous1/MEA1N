30af1d8ec7ab80b2d0539713f61a94c07667c0b5
Neous1 <Neous1@users.noreply.github.com>
2017-05-21 12:45:24 -0400

    using URL to define parameters

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,17 @@
 var hotelData = require("../data/hotel-data.json");
 
 module.exports.hotelsGetAll = function(req, res){
-        console.log("Get the json");
-        res
-            .status(200)
-            .json(hotelData);
+    console.log("Get the json");
+    res
+        .status(200)
+        .json(hotelData);
+};
+
+module.exports.hotelsGetOne = function(req, res){
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("Get hotelId", hotelId);
+    res
+        .status(200)
+        .json(thisHotel);
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,5 +7,9 @@ router
     .route("/hotels")
     .get(ctrHotels.hotelsGetAll); // mappping  controller to a route
 
+router
+    .route("/hotels/:hotelId")
+    .get(ctrHotels.hotelsGetOne);    
+
 module.exports=router;
 
