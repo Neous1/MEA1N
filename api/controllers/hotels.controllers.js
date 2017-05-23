2c645797f014f741fb29c0b8b442c9521fa5ed6b
Neous1 <Neous1@users.noreply.github.com>
2017-05-23 17:12:32 -0400

    Error trapping

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,5 +1,6 @@
 
 var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
 var Hotel = mongoose.model("Hotel");
 
 var runGeoQuery = function(req, res){
@@ -30,6 +31,7 @@ var runGeoQuery = function(req, res){
 module.exports.hotelsGetAll = function(req, res){
     var offset = 0;
     var count = 5;
+    var maxCount = 10;
 
     if(req.query && req.query.lat && req.query.lng){
         runGeoQuery(req,res);
@@ -43,32 +45,70 @@ module.exports.hotelsGetAll = function(req, res){
     if (req.query && req.query.count){
         count = parseInt(req.query.count, 10);
     }
+    if(isNaN(offset) || isNaN(count)) {
+        res
+            .status(400)
+            .json({
+                "message": "If supplied in querystring count and offset should be numbers"
+            });
+        return;
+    }
+
+    if(count > maxCount){
+        res
+            .status(400)
+            .json({
+                "message": "Count limit of " + maxCount + "exceeded"
+            });
+            return;
+    }
 
 Hotel
     .find()
     .skip(offset)
     .limit(count)    
     .exec(function(err, hotels){
+        if(err){
+            console.log("Error finding hotels");
+            res
+                .status(500)
+                .json(err);
+        }
+        else {
         console.log("Found hotels", hotels.length);
         res
             .json(hotels);
+        }
     });
 
 };
 
 module.exports.hotelsGetOne = function(req, res){
     var hotelId = req.params.hotelId;
-    // var thisHotel = hotelData[hotelId];
     console.log("Get hotelId", hotelId);
 
     Hotel
         .findById(hotelId)
         .exec(function(err,doc){ 
+            var response ={
+                status: 200,
+                message: doc
+            };
+        if(err){
+            console.log("Error finding hotel");
+            response.status=500;
+            response.message = err;
+        }
+        else if (!doc) {
+            response.status=404;
+            response.message = {
+                    "message": "Hotel ID not found"
+            };
+        }
         res
-            .status(200)
-            .json(doc);
-        })
-
+            .status(response.status)
+            .json(response.message);
+        });
 };
 
 
