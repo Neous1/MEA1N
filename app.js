20470da38fc48ec4ec84c742c47c6e579241e712
Neous1 <Neous1@users.noreply.github.com>
2017-05-31 09:55:12 -0400

    modified root app.js to capture form added fonctionalities to grab reviews on the app

--- app.js
+++ app.js
@@ -16,10 +16,14 @@ app.use(function(req, res, next){
 
 app.use(express.static(path.join(__dirname,"public")));
 app.use("/node_modules", express.static(__dirname +"/node_modules"));
+// app.use('/fonts', express.static(__dirname + '/fonts'));
 
 app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(bodyParser.json());
 
 app.use("/api", routes);
+
+
 
 var server = app.listen(app.get("port"), function(){
     var port = server.address().port;
