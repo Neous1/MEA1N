85766876c3f90cc3670bb18f6b3dac4f5edf9073
Neous1 <Neous1@users.noreply.github.com>
2017-06-05 16:35:54 -0400

    fixed a space before attribute that was throwing errorr

--- api/controllers/users.controllers.js
+++ api/controllers/users.controllers.js
@@ -31,8 +31,9 @@ module.exports.login = function(req, res){
     console.log("logging in user");
     var username = req.body.username;
     var password = req.body.password;
-    console.log(username)
-    console.log(req.body)
+    console.log("on loggin",username)
+    console.log("on loggin",password)
+
 
     User.findOne({
         username: username
@@ -41,56 +42,21 @@ module.exports.login = function(req, res){
             console.log(err)
             res.status(400).json(err);
         }
-        else{
-            console.log("user found", user)
-            res.status(200).json(user)
-        }
-        //  else{
-        //     // if(bcrypt.compareSync(password, user.password)){
-        //     if(bcrypt.compareSync(user.password)){
-        //         console.log("User found", user);
-        //         // var token = jwt.sign({ username: user.username}, "s3cr3t",{ expiresIn: 3600} );
-        //         // res.status(200).json({success: true, token: token});
-        //         res.status(200).json(user);
-            // }
-            // else{
-            //     res.status(401).json("Unauthorized");
-            // }
+
+         else{
+
+            if(bcrypt.compareSync(password,user.password)){
+                console.log("User found", user);
+                console.log(user.password)
+                // var token = jwt.sign({ username: user.username}, "s3cr3t",{ expiresIn: 3600} );
+                // res.status(200).json({success: true, token: token});
+                res.status(200).json(user);
+            }
+            else{
+                res.status(401).json("Unauthorized");
+            }
             
 
-        // }
+        }
     });
 };
-
-
-// //If user exists, they get to login to the website
-// module.exports.login = function(req, res) {
-//  console.log("Logging in user...");
-//  //Accessing the requests bodies
-//  var username = req.body.username;
-//  var password = req.body.password;
-//  console.log("Username: " + username);
-//  console.log("Password: " + password);
-//  console.log(req.body);
-
-//  User.findOne({
-//  username : username })
-//  .exec(function(err, user) {
-//  var hash = bcrypt.hashSync(password) //convert the req.body.password to hash values
-//  console.log(password)
-//  if(err) {
-//      //Login unsuccessfully
-//      console.log(err);
-//      res.status(400).json(err);
-//  } else {
-//      //Login successfully
-//      if(bcrypt.compareSync(password, hash)) {
-//      console.log("Login successfully", user);
-//      res.status(200).json(user);
-//  } else {
-//      console.log(user.password + " USER.PASSWORD");
-//      res.status(404).json("Unauthorized");
-//    }
-// }
-//  });
-// };
